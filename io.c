bbe2b1e5fddbe9fa653ccf4842f2f69d44f233c7
Zach Nickell <zach.x.nickell@gmail.com>
2019-02-03 13:48:11 -0600

    fix inline asm formatting

--- io.c
+++ io.c
@@ -13,13 +13,11 @@ unsigned char key[128] = {
 unsigned char inb(unsigned short port) {
     unsigned char value;
 
-    asm volatile ("inb %1, %0"
-                : "=a"(value)
-                : "Nd"(port));
+    asm volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
 
     return value;
 }
 
 void outb(unsigned short port, unsigned char value) {
-    asm volatile ( "outb %0, %1" : : "a"(value), "Nd"(port) );
+    asm volatile ("outb %0, %1" :: "a"(value), "Nd"(port));
 }
